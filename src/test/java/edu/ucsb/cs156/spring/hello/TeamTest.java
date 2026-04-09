a60b9416279c44550b7224a9f4595c69f2090e1c
Brian Hu <bjhu4481cobra@gmail.com>
2026-04-08 19:44:55 -0700

    Covered all tests / 100 mutation

--- src/test/java/edu/ucsb/cs156/spring/hello/TeamTest.java
+++ src/test/java/edu/ucsb/cs156/spring/hello/TeamTest.java
@@ -11,7 +11,8 @@ public class TeamTest {
 
     @BeforeEach
     public void setup() {
-        team = new Team("test-team");    
+        team = new Team("test-team");
+        
     }
 
     @Test
@@ -19,7 +20,27 @@ public class TeamTest {
        assert(team.getName().equals("test-team"));
     }
 
-   
+    @Test
+    public void toString_returns_correct_string() {
+        assertEquals("Team(name=test-team, members=[])", team.toString());
+    }
+
+    @Test
+    public void equals_returns_correct_boolean() {
+        assertEquals(true, team.equals(team));
+        Team t2 = new Team();
+        int[] testArr = new int[5];
+        assertEquals(false, team.equals(t2));
+        assertEquals(false, team.equals(testArr));
+    }
+
+    @Test
+    public void hash_testing() {
+        Team t = new Team();
+        int result = t.hashCode();
+        int expectedResult = 1;
+        assertEquals(result, expectedResult);
+    }
     // TODO: Add additional tests as needed to get to 100% jacoco line coverage, and
     // 100% mutation coverage (all mutants timed out or killed)
 
